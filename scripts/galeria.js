f33b0e3f7162f7b9e0d5e1e9dc3391cfb22b79a6
Pablo Rosas Marín <69369694+PabloR9080@users.noreply.github.com>
2021-10-26 17:46:13 -0500

    Funcion de agregar elementos creada

---
+++ scripts/galeria.js
@@ -0,0 +1,43 @@
+dog = {
+    "img": "",
+    "especie": "Perro",
+    "nombre": "Doki",
+    "disponible": true 
+};
+cat = {
+    "img": "",
+    "especie": "Gato",
+    "nombre": "Misho",
+    "disponible": true 
+};
+turtle = {
+    "img": "",
+    "especie": "Tortuga",
+    "nombre": "Rafael",
+    "disponible": false 
+};
+
+window.onload = function(){
+    principalGrid = document.getElementsByClassName("main")[0];
+    addAnimals(principalGrid);
+}
+
+addAnimals = function(cont){
+    animalesLista = [];
+    animalesLista.push(dog);
+    animalesLista.push(cat);
+    animalesLista.push(turtle);
+    console.log(`${animalesLista[0]["nombre"]}`);
+
+    animalesLista.forEach(animal => {
+        let divAnimal = document.createElement("div");
+        let animalSpecie = document.createElement("p");
+        let animalName = document.createElement("p");
+        animalSpecie.innerHTML = `Especie: ${animal["especie"]}`;
+        animalName.innerHTML = `Nombre: ${animal["nombre"]}`;
+        divAnimal.appendChild(animalSpecie);
+        divAnimal.appendChild(animalName);
+        divAnimal.className = "card animal";
+        cont.appendChild(divAnimal);
+    });
+}
