e1bcb15c2eea8bced5e4c98a1d4628ae380ad8a1
Lenin Rosas <leningaelrs@gmail.com>
2021-10-27 02:10:01 -0500

    Lista de mascotas con grid y responsive

--- scripts/galeria.js
+++ scripts/galeria.js
@@ -1,43 +1,46 @@
 dog = {
-    "img": "",
+    "img": "https://images.pexels.com/photos/8863636/pexels-photo-8863636.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     "especie": "Perro",
     "nombre": "Doki",
     "tamano": "grande"
 };
 cat = {
-    "img": "",
+    "img": "https://images.pexels.com/photos/4897323/pexels-photo-4897323.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     "especie": "Gato",
     "nombre": "Misho",
     "tamano": "chico"
 };
 turtle = {
-    "img": "",
-    "especie": "Tortuga",
+    "img": "https://images.pexels.com/photos/7053881/pexels-photo-7053881.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    "especie": "Perro",
     "nombre": "Rafael",
     "tamano": "chico"
 };
 
 window.onload = function(){
-    principalGrid = document.getElementsByTagName("main")[0];
+    const principalGrid = document.getElementById("catalogo");
     addAnimals(principalGrid);
 }
 
 addAnimals = function(cont){
-    animalesLista = [];
+    const animalesLista = [];
     animalesLista.push(dog);
     animalesLista.push(cat);
     animalesLista.push(turtle);
     console.log(`${animalesLista[0]["nombre"]}`);
 
     animalesLista.forEach(animal => {
-        let divAnimal = document.createElement("div");
-        let animalSpecie = document.createElement("p");
-        let animalName = document.createElement("p");
+        const divAnimal = document.createElement("div");
+        const animalImg = document.createElement("img");
+        const animalSpecie = document.createElement("p");
+        const animalName = document.createElement("p");
+        animalImg.src = `${animal["img"]}`;
         animalSpecie.innerHTML = `Especie: ${animal["especie"]}`;
         animalName.innerHTML = `Nombre: ${animal["nombre"]}`;
+        divAnimal.appendChild(animalImg);
         divAnimal.appendChild(animalSpecie);
         divAnimal.appendChild(animalName);
-        divAnimal.className = "card animal";
+        divAnimal.className = "card";
         cont.appendChild(divAnimal);
     });
 }
