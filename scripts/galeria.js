57be42fae0dc74957fa820b67851215e6ae463e8
Pablo Rosas Marín <69369694+PabloR9080@users.noreply.github.com>
2021-10-28 04:02:28 -0500

    Nuevo grid y barra de busqueda implementados

--- scripts/galeria.js
+++ scripts/galeria.js
@@ -1,26 +1,40 @@
-dog = {
+const dog = {
     "img": "https://images.pexels.com/photos/8863636/pexels-photo-8863636.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     "especie": "Perro",
     "nombre": "Doki",
     "tamano": "grande"
 };
-cat = {
+const cat = {
     "img": "https://images.pexels.com/photos/4897323/pexels-photo-4897323.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     "especie": "Gato",
     "nombre": "Misho",
     "tamano": "chico"
 };
-turtle = {
+const turtle = {
     "img": "https://images.pexels.com/photos/7053881/pexels-photo-7053881.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
     "especie": "Perro",
     "nombre": "Rafael",
     "tamano": "chico"
 };
 
-window.onload = function(){
-    const principalGrid = document.getElementById("catalogo");
+
+window.addEventListener('load',() =>{
+    const principalGrid = document.getElementById("grid");
     addAnimals(principalGrid);
-}
+    const grid = new Muuri('.grid', {
+        layout: {
+            rounding: false
+        }
+    });
+    grid.refreshItems().layout();
+    document.getElementById("grid").classList.add('imagenes-cargadas');
+
+    //Filtrado por barra de busqueda
+    document.querySelector('#barra-busqueda').addEventListener('input', (evento)=>{
+        const busqueda = evento.target.value;
+        grid.filter((item)=> item.getElement().dataset.etiquetas.includes(busqueda.toLowerCase()));
+    })
+})
 
 addAnimals = function(cont){
     const animalesLista = [];
@@ -31,16 +45,22 @@ addAnimals = function(cont){
 
     animalesLista.forEach(animal => {
         const divAnimal = document.createElement("div");
+        const divContent = document.createElement("div");
         const animalImg = document.createElement("img");
         const animalSpecie = document.createElement("p");
         const animalName = document.createElement("p");
+        let etiquetas = `${animal["especie"]} ${animal["tamano"]}`;
+        etiquetas = etiquetas.toLowerCase();
+        divAnimal.className = "item";
+        divContent.className = "item-contenido";
+        divAnimal.setAttribute("data-etiquetas",etiquetas);
         animalImg.src = `${animal["img"]}`;
         animalSpecie.innerHTML = `Especie: ${animal["especie"]}`;
         animalName.innerHTML = `Nombre: ${animal["nombre"]}`;
-        divAnimal.appendChild(animalImg);
-        divAnimal.appendChild(animalSpecie);
-        divAnimal.appendChild(animalName);
-        divAnimal.className = "card";
+        divContent.appendChild(animalImg);
+        divContent.appendChild(animalSpecie);
+        divContent.appendChild(animalName);
+        divAnimal.appendChild(divContent)
         cont.appendChild(divAnimal);
     });
 }
