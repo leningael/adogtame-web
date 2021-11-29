a3864ff22e5117d18e80385b0b73079d3535f90d
Lenin Rosas <leningaelrs@gmail.com>
2021-11-29 01:13:59 -0500

    Formulario usuario

--- resources/views/registro.blade.php
+++ resources/views/registro.blade.php
@@ -3,15 +3,7 @@
 @section('content')
     <div class="contenido">
         <div class="divContenidoDerecho">
-            <!--<h1>Datos Personales</h1><br>
-            <form action="" id="formulario">
-                <label for="nombre">Nombre</label>
-                <p><input type="text" name="nombre" id="nombre" placeholder="Escribe tu nombre" required></p>
-                <br><label for="email">Email</label>
-                <p><input type="email" name="email" id="email" placeholder="Escribe tu email"  required ></p>
-                <br><label for="telefono">Teléfono</label>
-                <p><input type="tel" name="telefono" id="telefono" placeholder="Escribe tu teléfono" required></p>-->
-            <form action="" id="formulario" method="post" enctype="multipart/form-data">
+            <form action="" id="formulario" class="formulario" method="post" enctype="multipart/form-data">
                 <br><h1>Datos de la Mascota</h1>
                 <br><label for="nombreusuario">Nombre de usuario</label>
                 <p><input type="text" name="nombreusuario" id="nombre-usuario" placeholder="Escribe el nombre de usuario" required></p>
@@ -41,6 +33,27 @@
                 <br><label for="descripcion">Descripción</label>
                 <br><p><textarea name="descripcion" id="descripcion-mascota" placeholder="Escribe una descripción de tu mascota"></textarea></p>
                 <br><p class="sendButton"><button type="button" id="enviar">Enviar</button></p>
+            </form>
+            <br><br><hr><br><br>
+            <form action="" class="formulario">
+                <h1>Datos Personales</h1><br>
+                <label for="nombreusuario">Nombre de usuario</label>
+                <p><input type="text" name="nombreusuario" id="username-usuario" placeholder="Escribe el nombre de usuario" required></p><br>
+                <label for="nombre">Nombre</label>
+                <p><input type="text" name="nombre" id="nombre-usuario" placeholder="Escribe tu nombre" required></p>
+                <br><label for="email">Email</label>
+                <p><input type="email" name="email" id="email-usuario" placeholder="Escribe tu email"  required ></p>
+                <br><label for="telefono">Teléfono</label>
+                <p><input type="tel" name="telefono" id="telefono-usuario" placeholder="Escribe tu teléfono" required></p>
+                <br><label for="estado">Estado:</label>
+                <select name="estado" id="estado-usuario" required>
+                    <option value="Yucatán">Yucatán</option>
+                    <option value="Quintana Roo">Quintana Roo</option>
+                    <option value="Campeche">Campeche</option>
+                </select><br>
+                <br><label for="municipio">Municipio</label>
+                <p><input type="text" name="municipio" id="municipio-usuario" placeholder="Escribe tu municipio" required></p>
+                <br><p class="sendButton"><button type="button" id="enviarUsuario">Enviar</button></p>
             </form>
         </div>
         <div class="contenedorImagen">
